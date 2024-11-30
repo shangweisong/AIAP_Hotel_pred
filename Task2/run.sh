ecc8e5169f778e2291726263190729edc9fa5669
shangwei <shangweisong@gmail.com>
2024-11-30 18:59:52 +0800

    debugged run.sh

--- Task2/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# # Navigate to the directory containing Main.py (optional if you already run this script from the directory)
-# cd "$(dirname "$0")"
-
-# Run the Python script
-python Main.py
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to the directory containing Main.py (optional if you already run this script from the directory)
+cd "$(dirname "$0")"
+
+# Run the Python script
+python3 Task2/src/main.py
+
