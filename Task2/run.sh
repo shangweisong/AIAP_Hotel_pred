cbb6b7d2d21cf9d4a0b776444343892b04c0f787
shangwei <shangweisong@gmail.com>
2024-11-30 18:49:36 +0800

    add run.sh script

---
+++ Task2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# # Navigate to the directory containing Main.py (optional if you already run this script from the directory)
+# cd "$(dirname "$0")"
+
+# Run the Python script
+python Main.py
