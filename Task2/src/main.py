5e75d5d7746d50873d994650b3b7bae3fb1768d8
shangwei <shangweisong@gmail.com>
2024-11-30 18:48:33 +0800

    load db

--- Task2/src/main.py
+++ Task2/src/main.py
@@ -1,2 +1,9 @@
 import sqlite3
 import pandas as pd
+from setup import load_config
+
+# load configuration 
+config = load_config()
+
+db_path = config['database']['db_path']
+conn= sqlite3.connect(db_path)
