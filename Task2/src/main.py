7546c08f7d4ca59021d353b82a747cd28c2831dd
shangwei <shangweisong@gmail.com>
2024-11-30 19:14:53 +0800

    load db to a df

--- Task2/src/main.py
+++ Task2/src/main.py
@@ -12,3 +12,10 @@ config = load_config()
 
 db_path = config['database']['db_path']
 conn= sqlite3.connect(db_path)
+# Find table name 
+tablename_query = "SELECT name FROM sqlite_master WHERE type='table';"
+tablename= pd.read_sql_query(tablename_query, conn)
+
+# Get data from database
+df_query = f"SELECT * FROM {tablename.iloc[0,0]};"
+df= pd.read_sql_query(df_query, conn)
